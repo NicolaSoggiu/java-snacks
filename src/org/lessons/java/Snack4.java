d75f7efd3f2a83c7b50c60749c8431be88624687
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-10-19 15:31:19 +0200

    add Snack4

--- src/org/lessons/java/Snack4.java
+++ src/org/lessons/java/Snack4.java
@@ -1,4 +1,34 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack4 {
+
+    /**
+     * Data in input una stringa verificare se è palindroma
+     */
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Inserisci una parola e scopri se è palindroma!");
+        String word = scan.nextLine();
+
+        char[] chars = word.toCharArray();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] != chars[chars.length - 1 - i]) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("La parola " + word +" è palindroma!");
+        } else {
+            System.out.println("La parola non è palindroma!");
+        }
+        scan.close();
+    }
 }
