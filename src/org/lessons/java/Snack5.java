2937b9c3a875b047b0870c33634a6d47a6232717
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-10-19 16:06:04 +0200

    add Snack6

--- src/org/lessons/java/Snack5.java
+++ src/org/lessons/java/Snack5.java
@@ -1,4 +1,17 @@
 package org.lessons.java;
 
 public class Snack5 {
+
+    /**
+     * Dato un numero sotto forma di stringa, convertirlo in intero senza utilizzare funzioni già pronte.
+     * Possibile usare solo :
+     * cicli
+     * chartAt
+     * if / switch
+     * Es. “25" come stringa deve essere convertito in intero 25.
+     */
+
+    public static void main(String[] args) {
+
+    }
 }
--- src/org/lessons/java/Snack6.java
+++ src/org/lessons/java/Snack6.java
@@ -1,4 +1,30 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack6 {
+
+    /**
+     * Scrivere un programma che dati dei secondi li converta in ore, minuti, secondi;
+     * e mostri a video la stringa generata (secondi → “hh:mm:ss”)
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Inserisci i secondi che vuoi convertire: ");
+        // secondi da convertire
+        int initialSeconds = scan.nextInt();
+
+        // calcolo
+        int hours = initialSeconds / 3600;
+        initialSeconds %= 3600;
+        int minutes = initialSeconds / 60;
+        int seconds = initialSeconds % 60;
+
+        // metto tutto in una stringa
+        String convertedTime = String.format("%02d:%02d:%02d", hours, minutes, seconds);
+
+        // stampo il risultato
+        System.out.println("Tempo convertito: " + convertedTime);
+    }
 }
