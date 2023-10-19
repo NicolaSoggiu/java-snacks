cbc3680d5d69921c8a4820084250d53f41bd26bb
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-10-19 17:01:50 +0200

    add Snack5

--- src/org/lessons/java/Snack5.java
+++ src/org/lessons/java/Snack5.java
@@ -11,7 +11,16 @@ public class Snack5 {
      * Es. “25" come stringa deve essere convertito in intero 25.
      */
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
+            String numberStr = "123456789";
+            int numberInt = 0;
+
+            for (int i = 0; i < numberStr.length() ; i++) {
+                char charNumber = numberStr.charAt(i);
+                int convertedNumber = charNumber - '0';
+                numberInt = numberInt * 10 + convertedNumber;
+            }
+            System.out.println(numberInt);
+        }
     }
-}
